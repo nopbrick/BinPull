7830ea149340991df72efd63418c348da4c72346
Filip Ceglik <fceglik@gmail.com>
2021-12-28 16:10:24 +0100

    Upload

---
+++ BinPull/Invoker.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace BinPull
+{
+    internal class Invoker
+    {
+        public static Assembly Invoke(byte[] bin)
+        {
+            var asm = Assembly.Load(bin);
+            return asm;
+        }
+
+        public static void Run(Assembly assembly, string[] param)
+        {
+            assembly.EntryPoint.Invoke(null, new object[] { param });
+        }
+    }
+}
---
+++ BinPull/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BinPull
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dummy = new List<string>(args);
+            dummy.RemoveRange(0, 1);
+            Invoker.Run(Invoker.Invoke(Prog.LoadURL(args[0])), dummy.ToArray());
+        }
+    }
+}
